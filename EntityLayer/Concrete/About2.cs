48a40d12fd55a30ae03da8ec52d751ffbf340e43
Cloutout <75390041+Cloutout@users.noreply.github.com>
2023-07-01 19:12:49 +0300

    Entityler olusturuldu ve paketler yuklendi

---
+++ EntityLayer/Concrete/About2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About2
+    {
+        public int About2Id { get; set; }
+        public int Title1 { get; set; }
+        public int Title2 { get; set; }
+        public int Description { get; set; }
+        public int Image { get; set; }
+
+    }
+}
