318ca3c03296a31c534b1e5afc7fdf76fb556c60
Cloutout <75390041+Cloutout@users.noreply.github.com>
2023-07-04 15:20:36 +0300

    Validation rule ve tema
    Bir kaç validations rule tanımlandı ve tema paketi yüklendi

---
+++ NetCoreMVC/Controllers/DefaultController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMVC.Controllers
+{
+    public class DefaultController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
